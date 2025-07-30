2e3cb465b3869a776b33242ab0e886236607392e
William Roberts <werobert@yahoo.com>
2025-07-30 13:30:50 -0400

    db init

--- tests/db/db.go
+++ tests/db/db.go
@@ -1,3 +1,19 @@
 package db
 
-import ()
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var DB *sql.DB
+
+func InitDB() {
+	DB, error := sql.Open("sqlite3", "api.db")
+
+	if error != nil {
+		panic("couldnt connect to DB")
+	}
+
+	DB.SetMaxOpenConns(10)
+	DB.SetMaxIdleConns(5)
+}
