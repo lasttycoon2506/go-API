6a933db09ea48c3d384b52197e27ee5060c80948
William Roberts <werobert@yahoo.com>
2025-07-30 12:14:25 -0400

    createEvent()

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"example.com/m/v2/models"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,10 +11,21 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 	server.Run(":8080")
 
 }
 
 func getEvents(context *gin.Context) {
 	context.JSON(http.StatusOK, gin.H{"testing": "working"})
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindBodyWithJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "error parsing data"})
+		return
+	}
 }
