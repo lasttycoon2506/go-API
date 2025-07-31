17704be337404e0f91433a61d9f78f8dcad49b37
William Roberts <werobert@yahoo.com>
2025-07-31 14:31:17 -0400

    param path upd

--- routes/routes.go
+++ routes/routes.go
@@ -4,6 +4,6 @@ import "github.com/gin-gonic/gin"
 
 func RegisterRoutes(server *gin.Engine) {
 	server.GET("/events", getEvents)
-	server.GET("/events:id", getEvent)
+	server.GET("/events/:id", getEvent)
 	server.POST("/events", createEvent)
 }
