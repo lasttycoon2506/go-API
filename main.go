0a6b220987dde3150aef62d4c22339465053b055
William Roberts <werobert@yahoo.com>
2025-07-31 14:18:12 -0400

    main refactor

--- main.go
+++ main.go
@@ -1,69 +1,14 @@
 package main
 
 import (
-	"net/http"
-	"strconv"
-
 	"example.com/m/v2/db"
-	"example.com/m/v2/models"
+	"example.com/m/v2/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	db.InitDB()
 	server := gin.Default()
-
-	server.GET("/events", getEvents)
-	server.GET("/events:id", getEvent)
-	server.POST("/events", createEvent)
+	routes.RegisterRoutes(server)
 	server.Run(":8080")
-
-}
-
-func getEvent(context *gin.Context) {
-	id, err := strconv.ParseInt(context.Param("id"), 10, 64)
-
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "error retrieving data", "error": err})
-		return
-	}
-
-	event, err := models.GetEvent(id)
-
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "error retrieving data", "error": err})
-		return
-	}
-
-	context.JSON(http.StatusOK, event)
-}
-
-func getEvents(context *gin.Context) {
-	events, err := models.GetAllEvents()
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "error retrieving data", "error": err})
-		return
-	}
-	context.JSON(http.StatusOK, events)
-}
-
-func createEvent(context *gin.Context) {
-	var event models.Event
-	err := context.ShouldBindBodyWithJSON(&event)
-
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "error parsing data", "error": err})
-		return
-	}
-
-	event.ID = 1
-	event.UserId = 1
-	err = event.Save()
-
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "error saving data", "error": err})
-		return
-	}
-
-	context.JSON(http.StatusCreated, gin.H{"message": "event created", "event": event})
 }
