5d2341df0fc51e58fa929cc57d068251e6971910
William Roberts <werobert@yahoo.com>
2025-08-01 14:21:53 -0400

    login route

--- routes/routes.go
+++ routes/routes.go
@@ -9,4 +9,5 @@ func RegisterRoutes(server *gin.Engine) {
 	server.PUT("/events/:id", editEvent)
 	server.DELETE("/events/:id", deleteEvent)
 	server.POST("/register", register)
+	server.POST("/login", login)
 }
