ad6a0ba2b9634faf3479f3aa8344353073285512
William Roberts <werobert@yahoo.com>
2025-08-01 14:20:34 -0400

    check hashedPw()

--- utils/hash.go
+++ utils/hash.go
@@ -6,3 +6,8 @@ func HashPassword(password string) (string, error) {
 	hashedPwInBytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
 	return string(hashedPwInBytes), err
 }
+
+func CheckHashedPassword(password, hashedPassword string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	return err == nil
+}
