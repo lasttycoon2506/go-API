16bb9fb7ae8bf7085bbe92fc8ef911308255e4c1
William Roberts <werobert@yahoo.com>
2025-08-01 13:08:49 -0400

    prep insertUser query

--- models/user.go
+++ models/user.go
@@ -1,15 +1,21 @@
 package models
 
+import "example.com/m/v2/db"
+
 type User struct {
 	ID       int64
 	email    string `binding:"required"`
 	password string `binding:"required"`
 }
 
-func (u User) Save() {
+func (u User) Save() error {
 	query := `
 		INSERT INTO users (email, password)
 		VALUES (?, ?)
 	`
-
+	statement, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer statement.Close()
 }
