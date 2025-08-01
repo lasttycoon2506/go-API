871e5457e5228393cb34ca8d91d804fe7add9028
William Roberts <werobert@yahoo.com>
2025-08-01 13:18:53 -0400

    user route

---
+++ routes/users.go
@@ -0,0 +1,25 @@
+package routes
+
+import (
+	"net/http"
+
+	"example.com/m/v2/models"
+	"github.com/gin-gonic/gin"
+)
+
+func register(context *gin.Context) {
+	var user models.User
+	err := context.ShouldBindBodyWithJSON(&user)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "error parsing data", "error": err})
+		return
+	}
+
+	err = user.Save()
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "error saving data", "error": err})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "user created", "user": user})
+}
