c59ae96eb7b7d7c9346df07d3fa14142ce8fb68f
William Roberts <werobert@yahoo.com>
2025-08-02 12:54:45 -0400

    verify token

---
+++ utils/token.go
@@ -0,0 +1,43 @@
+package utils
+
+import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+const secretKey = "secretcheck"
+
+func CreateToken(email string, userId int64) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"email":  email,
+		"userId": userId,
+		"exp":    time.Now().Add(time.Hour).Unix(),
+	})
+
+	return token.SignedString([]byte(secretKey))
+}
+
+func VerifyToken(token string) error {
+	parsedToken, err := jwt.Parse(token, func(t *jwt.Token) (any, error) {
+		_, ok := t.Method.(*jwt.SigningMethodHMAC)
+
+		if !ok {
+			return nil, errors.New("signing method didnt match")
+		}
+
+		return secretKey, nil
+	})
+
+	if err != nil {
+		return errors.New("couldnt parse token")
+	}
+
+	tokenIsValid := parsedToken.Valid
+	if !tokenIsValid {
+		return errors.New("invalid token")
+	}
+
+	return nil
+}
