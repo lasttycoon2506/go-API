727f2e27eb963ac5ad7bb51dd5c2f7d93bc62e5d
William Roberts <werobert@yahoo.com>
2025-08-04 11:32:28 -0400

    auth-middleware to routes

--- routes/routes.go
+++ routes/routes.go
@@ -1,13 +1,16 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"example.com/m/v2/middleware"
+	"github.com/gin-gonic/gin"
+)
 
 func RegisterRoutes(server *gin.Engine) {
 	server.GET("/events", getEvents)
 	server.GET("/events/:id", getEvent)
-	server.POST("/events", createEvent)
-	server.PUT("/events/:id", editEvent)
-	server.DELETE("/events/:id", deleteEvent)
+	server.POST("/events", middleware.Authenticate, createEvent)
+	server.PUT("/events/:id", middleware.Authenticate, editEvent)
+	server.DELETE("/events/:id", middleware.Authenticate, deleteEvent)
 	server.POST("/register", register)
 	server.POST("/login", login)
 }
