074b872e701bb118234c7cbf25f2e1465aa03993
William Roberts <werobert@yahoo.com>
2025-08-11 16:21:51 -0400

    added cors

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"example.com/m/v2/db"
 	"example.com/m/v2/routes"
 	"github.com/gin-gonic/gin"
@@ -9,6 +11,13 @@ import (
 func main() {
 	db.InitDB()
 	server := gin.Default()
+	server.Use(func(context *gin.Context) {
+		origin := context.GetHeader("Origin")
+		if origin != "http://localhost:3000" || origin == "" {
+			context.AbortWithStatusJSON(http.StatusForbidden, gin.H{"error": "Forbidden: Invalid Origin"})
+			return
+		}
+	})
 	routes.RegisterRoutes(server)
 	server.Run(":8080")
 }
