ff236f25917fa61098563766a8381a58332c76e5
William Roberts <werobert@yahoo.com>
2025-08-12 16:47:28 -0400

    cors headers

--- main.go
+++ main.go
@@ -10,14 +10,30 @@ import (
 
 func main() {
 	db.InitDB()
+
 	server := gin.Default()
 	server.Use(func(context *gin.Context) {
 		origin := context.GetHeader("Origin")
-		if origin != "http://localhost:3000" || origin == "" {
+
+		if context.Request.Method == "OPTIONS" && origin == "http://localhost:3000" {
+			context.Header("Access-Control-Allow-Origin", "http://localhost:3000")
+			context.Header("Access-Control-Allow-Headers", "Content-Type")
+			context.Header("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT")
+			context.Status(http.StatusOK)
+			context.Abort()
+			return
+		}
+
+		if (origin != "http://localhost:3000" && origin != "http://localhost:8080") || origin == "" {
 			context.AbortWithStatusJSON(http.StatusForbidden, gin.H{"error": "Forbidden: Invalid Origin"})
 			return
 		}
+
+		context.Header("Access-Control-Allow-Origin", "http://localhost:3000")
+		context.Header("Access-Control-Allow-Headers", "Content-Type")
+		context.Header("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT")
 	})
+
 	routes.RegisterRoutes(server)
 	server.Run(":8080")
 }
